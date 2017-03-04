169188926f4ea53c55eef67282a851acc049d34c
Jonathan Fu <fujianglai@gmail.com>
2017-03-04 22:39:30 +1100

    change to use local data

--- services/qantas.js
+++ services/qantas.js
@@ -1,4 +1,7 @@
 
+var data = require('../data/airports');
+
 exports.getAllData = function () {
-  return require('./apiHandler').call(null, require('../config/config').QANTAS_API_URL);
+  // return require('./apiHandler').call(null, require('../config/config').QANTAS_API_URL);
+  return Promise.resolve(data) ;
 };
